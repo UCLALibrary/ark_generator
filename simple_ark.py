cc8ef5f808305ea093de5c0a522cd8f62cedca5a
aprigge <34147754+aprigge@users.noreply.github.com>
2019-11-22 08:50:32 -0800

    Changed to use batch-register.py

--- simple_ark.py
+++ simple_ark.py
@@ -3,33 +3,28 @@ import os
 import subprocess
 import pandas as pd
 
-
 #creates a mappings file that provides metadata to EZID
 def create_mappings():
 
-    title = row['P_number']
+    
     mappings_file = open("mappings.txt", "w+")
-    string = 'erc.who: UCLA Library', ('\nerc.what: '+str(title))
-    for s in string:
-        mappings_file.write(s)
-
+    string = 'erc.who=UCLA Library'
+    mappings_file.write(string)
 
 works_file = raw_input('File:')
 ark_shoulder = raw_input('ARK shoulder:')
-ezid_input = raw_input('EZID username and password:')
 parent_ark_list = []
 output_file = 'ark_export.csv'
-works_cursor = csv.DictReader(open(works_file),
-    delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-for row in works_cursor:
-    create_mappings()
-    cmd_ezid = ['python', 'ezid.py', ezid_input, 'mint', ark_shoulder, '@', 'mappings.txt']
-    parent_ark = subprocess.Popen(cmd_ezid, stdout=subprocess.PIPE).communicate()[0]
-    parent_ark = (str(parent_ark)).replace('success: ', '')
-    parent_ark_list.append(parent_ark)
-    
+create_mappings()
+cmd_ezid = ['python', 'batch-register.py', '-c', 'ucla-library', '-s', ark_shoulder, 'mint', 'mappings.txt', works_file]
+parent_ark = subprocess.Popen(cmd_ezid, stdout=subprocess.PIPE).communicate()[0]
+parent_ark = (str(parent_ark))
+items_list = parent_ark.split(',')
+for i in items_list:
+    if 'ark:/' in i:
+        parent_ark_list.append(i)
+df_list = parent_ark_list[1:]
 data= pd.read_csv(works_file, sep=',', delimiter=None, header='infer')
 data = data.drop("ark_number", axis=1)
-data.insert(2,'ark_number', parent_ark_list)
+data.insert(2,'ark_number', df_list)
 data.to_csv(path_or_buf=output_file, sep=',', na_rep='', float_format=None, index=False)
